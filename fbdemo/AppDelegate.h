f497e82b1f8f3228f6d5b7b11f16c925b4a60f81
Gabriel Valdivia <valdivia.gabriel@gmail.com>
2014-06-21 15:20:45 -0700

    Initial Commit

---
+++ fbdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fbdemo
+//
+//  Created by Gabriel Valdivia on 6/21/14.
+//  Copyright (c) 2014 Gabriel Valdivia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
